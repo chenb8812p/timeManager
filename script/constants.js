a069a8efd2f288209ddf5e30f0227c52fb12fcc3
yih <598364654@qq.com>
2016-04-03 19:53:13 +0800

    4-3

--- script/constants.js
+++ script/constants.js
@@ -4,10 +4,10 @@ var WEBSITE = HOST + '/json/';
 
 http://static.time.dev.net/json/
 //请求时间分配百分比
-var time_p = WEBSITE + 'time_p.json';
+var TIME_P = WEBSITE + 'time_p.json';
 // login
 var LOGOIN =  WEBSITE + 'login.json';
 //register
 var REGISTER = WEBSITE + 'register.json';
-// 用户id
-var userId = localStorage.getItem('userId');
+//请求排名
+var RANKING = WEBSITE + 'ranking.json';
